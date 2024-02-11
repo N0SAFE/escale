589840e2a53bb192ab478fbc3fe584d3483caec1
n0safe <sssebillemathis@gmail.com>
2024-02-11 20:32:14 +0100

    update navbar and remove server.pid from github

--- apps/front/src/components/Navbar.tsx
+++ apps/front/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <nav className="bg-[#8B5A3C] pt-16 pb-16">
+        <nav className="bg-[#8B5A3C] md:py-16">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
